62ef729739f63f89480b7037d0c4d1731fe6e679
Andrew Tompkins <103470886+Andrewtomp@users.noreply.github.com>
2022-04-12 21:37:52 -0500

    Added deployer.js
    deployer.js added virus to all rooted servers on the network and targets a single server

---
+++ deployer.js
@@ -0,0 +1,39 @@
+/** 
+ * script places virus on all servers rooted servers on the network.
+ * all scripts target one server passed in args
+ * 
+ * @param {NS} ns */
+export async function main(ns) {
+
+	var virus = "virus1.js";
+	var target = ns.args[0];
+
+	let servers = [];
+	let serversToScan = ns.scan("home");
+	while (serversToScan.length > 0) {
+		let server = serversToScan.shift();
+		if (!servers.includes(server) && server !== "home") {
+			servers.push(server);
+			serversToScan = serversToScan.concat(ns.scan(server));
+		}
+		if (ns.hasRootAccess(server)) {
+			let maxThreads = Math.floor(ns.getServerMaxRam(server) / ns.getScriptRam(virus));
+			if (server != "home") {
+				await ns.scp(virus, server);
+				ns.killall(server);
+				if (maxThreads > 0) {
+					ns.exec(virus, server, maxThreads, target);
+				}
+			}
+			else {
+				let freeThreads = Math.floor((ns.getServerMaxRam("home") - ns.getServerUsedRam("home")) / ns.getScriptRam(virus));
+				if (freeThreads > 0) {
+					ns.exec(virus, "home", freeThreads, target);
+				}
+
+			}
+		}
+	}
+
+
+}
