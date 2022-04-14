134e3a0531f3b0157ca35a60a18429413337f708
Andrew Tompkins <103470886+Andrewtomp@users.noreply.github.com>
2022-04-13 19:05:59 -0500

    Cleaned up servAdd.js

--- servAdd.js
+++ servAdd.js
@@ -8,7 +8,7 @@ export async function main(ns) {
 		"relaySMTP.exe": ns.relaysmtp,
 		"HTTPWorm.exe": ns.httpworm,
 		"SQLInject.exe": ns.sqlinject
-	}
+	};
 
 	function scriptsOwned() {
 		return Object.keys(scripts).filter(function (file) {
@@ -17,38 +17,33 @@ export async function main(ns) {
 	}
 
 	function canAdd(server) {
-		return scriptsOwned(server) >= ns.getServerNumPortsRequired(server)
+		return scriptsOwned(server) >= ns.getServerNumPortsRequired(server);
 	}
 
-	function addServ(server) {
+	async function addServ(server) {
 		for (var file of Object.keys(scripts)) {
-			if (ns.fileExists(file, home)) {
+			if (ns.fileExists(file, "home")) {
 				var runScript = scripts[file];
-				runScript(server);
+				await runScript(server);
 			}
 		}
-		ns.nuke(server);
+		await ns.nuke(server);
 	}
-	async function netScan() {
-		let servers = [];
-		let serversToScan = ns.scan("home");
-		while (serversToScan.length > 0) {
-			let server = serversToScan.shift();
-			if (!servers.includes(server) && server !== "home") {
-				servers.push(server);
-				serversToScan = serversToScan.concat(ns.scan(server));
-			}
-			if (!ns.hasRootAccess(server)) {
-				if (canAdd(server)) {
-					await addServ(server);
-					ns.tprint("Added: " + server);
-				}
+
+	let servers = [];
+	let serversToScan = ns.scan("home");
+	while (serversToScan.length > 0) {
+		let server = serversToScan.shift();
+		if (!servers.includes(server) && server !== "home") {
+			servers.push(server);
+			serversToScan = serversToScan.concat(ns.scan(server));
+			if (!ns.hasRootAccess(server) && canAdd(server)) {
+
+				await addServ(server);
+				ns.tprint("Added: " + server);
 			}
 		}
 
 	}
-
-	netScan();
-
 
 }
